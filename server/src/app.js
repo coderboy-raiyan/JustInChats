4548d23e0f56a38303fd12edd07d664eb9bbd3c3
coderboy-raiyan <tajkierhaque@gmail.com>
2026-06-21 12:07:33 +0600

    set up done

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,8 @@
 import express from "express";
 const app = express();
 
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true, message: "Server is healthy" });
+});
+
 export default app;
